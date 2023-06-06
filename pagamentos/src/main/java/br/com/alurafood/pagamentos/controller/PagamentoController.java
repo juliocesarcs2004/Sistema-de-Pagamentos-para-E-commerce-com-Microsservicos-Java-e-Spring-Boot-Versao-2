b0245ee5d7c3478376d9d34a81d7a5b0cfc32ff5
Julio César <juliocesarcs2004@gmail.com>
2023-06-05 22:10:52 -0300

    Realizada configuração de Migrations e testes loacais no projeto

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -41,13 +41,13 @@ public class PagamentoController {
         return ResponseEntity.created(endereco).body(pagamento);
     }
 
-    @PutMapping
+    @PutMapping("/{id}")
     public ResponseEntity<PagamentoDto> atualizar(@PathVariable @NotNull Long id, @RequestBody @Valid PagamentoDto dto) {
         PagamentoDto atualizado = service.atualizarPagamento(id, dto);
         return ResponseEntity.ok(atualizado);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     public ResponseEntity<PagamentoDto> remover(@PathVariable @NotNull Long id) {
         service.excluirPagamento(id);
         return ResponseEntity.noContent().build();
--- pagamentos/src/main/java/br/com/alurafood/pagamentos/model/Pagamento.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/model/Pagamento.java
@@ -28,19 +28,15 @@ public class Pagamento {
     @Positive
     private BigDecimal valor;
 
-    @NotBlank
     @Size(max=100)
     private String nome;
 
-    @NotBlank
     @Size(max=19)
     private String numero;
 
-    @NotBlank
     @Size(max=7)
     private String expiracao;
 
-    @NotBlank
     @Size(min=3, max=3)
     private String codigo;
 
