a480441e04d21f4338082cb1bdcc14f2a7226e94
Julio César <juliocesarcs2004@gmail.com>
2023-06-14 19:43:43 -0300

    Configurado o microsservico de pagamentos como client

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
@@ -2,6 +2,7 @@ package br.com.alurafood.pagamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
