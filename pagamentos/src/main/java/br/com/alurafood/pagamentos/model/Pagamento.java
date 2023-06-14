fabb67e2c8930bdb9b9b192111ae817bcd8b4efc
Julio César <juliocesarcs2004@gmail.com>
2023-06-14 19:22:30 -0300

    Criado o servidor

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/model/Pagamento.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/model/Pagamento.java
@@ -1,7 +1,6 @@
 package br.com.alurafood.pagamentos.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
